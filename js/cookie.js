caaaea073be61abc3b2f8077e733fef55a0d0c57
barzyk1997 <barzyk1997@gmail.com>
2018-01-18 21:09:18 +0100

    not working but close

--- js/cookie.js
+++ js/cookie.js
@@ -6,19 +6,48 @@
 */
 
 const cookieParser = require('cookie-parser');
+const pgp = require('pg-promise')({});
+const cn = 'postgres://postgres:dawid@localhost:5432/postgres';
+const db = pgp(cn);
+
 
 module.exports = ({
     authorize : function (req,res,next) {
         if ( req.cookies.user ) {
-            req.user = req.cookies.user; 
+            var usr = req.cookies.user.split('&');
+            req.user = {
+                login : usr[0],
+                username : usr[1],
+                password : usr[2],
+                wins : usr[3],
+                losses : usr[4],
+                draws : usr[5]
+            } 
             next();
         } else {
             res.redirect( '/login'); 
         }
     },
     init : function(io, app) {
-        function checkPwd(name,pwd) {
-            return true;
+        function checkPwd(login, pwd, res)
+        {
+            db.task(t => {
+                return t.one('SELECT * FROM users WHERE login = $1', login);
+            }).then(result => {
+                console.log(result);
+                if(result.password != pwd) 
+                {
+                    return false;
+                } else {
+                    res.cookie('user', result.login + '&' + result.username + '&' + result.password + '&' + result.wins + '&' + result.losses + '&' + result.draws);
+                    console.log('noice');
+                    return true;
+                }
+            }).catch(err => {
+                console.error('', err)
+                return false;
+            });
+
         };
         function allowedUsername(name, pwd, pwd2) {
             if(pwd != pwd2){
@@ -37,8 +66,7 @@ module.exports = ({
         app.post('/login', (req, res) => {
             var user = req.body.username;
             var pwd = req.body.pwd;
-            if(checkPwd(user,pwd)) {
-                res.cookie('user', user);
+            if(checkPwd(user,pwd,res)) {
                 res.redirect('/');
             } else {
                 res.render('login', { message : 'Incorrect username or password' });
